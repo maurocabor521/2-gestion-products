9faedf86bcc452f7a2726163d7eeff4e7a3a791d
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2020-02-06 11:19:05 -0500

    Instalacion FOSUserBundle

---
+++ src/Form/ProductType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ProductType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('name')
+                ->add('description')
+                ->add('price')
+                ->add('category', EntityType::class, [
+                    'class' => Category::class,
+                    'choice_label' => 'name'
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+
+}
